e6ff63ccbe32efd9c033857bf72b16c702cd40c6
NasDevFe <85785901+NasDevFe@users.noreply.github.com>
2022-03-31 13:16:59 +0530

    Edited the Array Values

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-var messageArray = ['This is the Type-Writer Effect']
+var messageArray1 = ['I am a Front-End Developer']
+var messageArray2 = ['I Love Creating Responsive Websites']
+var messageArray3 = ['Using HTML,CSS and Javascript']
+var messageArray4 = ['I am proficient in CSS Libraries']
+var messageArray5 = ['I am Currently Learning React']
+var messageArray6 = ['I Aspire to be a MERN Developer in Future']
 var textPosition = 0;
 var speed = 100;
 
 
 typewriter1=()=>{
-  document.querySelector("#message1").innerHTML = messageArray[0].substring(0,textPosition)
+  document.querySelector("#message1").innerHTML = messageArray1[0].substring(0,textPosition)
   + "<span>\u25ae</span>"
 
-  if(textPosition++ != messageArray[0].length)
+  if(textPosition++ != messageArray1[0].length)
     setTimeout(typewriter1 , speed)
 
 }
@@ -16,10 +21,10 @@ window.addEventListener("load", typewriter1);
 
 
 typewriter2=()=>{
-  document.querySelector("#message2").innerHTML = messageArray[0].substring(0,textPosition)
+  document.querySelector("#message2").innerHTML = messageArray2[0].substring(0,textPosition)
   + "<span>\u25ae</span>"
 
-  if(textPosition++ != messageArray[0].length)
+  if(textPosition++ != messageArray2[0].length)
     setTimeout(typewriter2 , speed)
 
 }
@@ -30,22 +35,22 @@ window.addEventListener("load", typewriter2);
 
 
 function typewriter3(){
-  document.querySelector("#message3").innerHTML = messageArray[0].substring(0,textPosition)
+  document.querySelector("#message3").innerHTML = messageArray3[0].substring(0,textPosition)
   + "<span>\u25ae</span>"
 
-for(i=0 ; i<=messageArray[0].length; i++){
-    messageArray[0].substring(0,i)}
+for(i=0 ; i<=messageArray3[0].length; i++){
+    messageArray3[0].substring(0,i)}
     setTimeout(typewriter3, speed)
 
 }
 window.addEventListener("load", typewriter3);
 
 function typewriter4(){
-  document.querySelector("#message4").innerHTML = messageArray[0].substring(0,textPosition)
+  document.querySelector("#message4").innerHTML = messageArray4[0].substring(0,textPosition)
   + "<span>\u25ae</span>"
 
-for(i=0 ; i<=messageArray[0].length; i++){
-    messageArray[0].substring(0,i)}
+for(i=0 ; i<=messageArray4[0].length; i++){
+    messageArray4[0].substring(0,i)}
     setTimeout(typewriter4, speed)
 
 }
@@ -53,12 +58,12 @@ window.addEventListener("load", typewriter4);
 
 
 function typewriter5(){
-  document.querySelector("#message5").innerHTML = messageArray[0].substring(0,textPosition)
+  document.querySelector("#message5").innerHTML = messageArray5[0].substring(0,textPosition)
   + "<span>\u25ae</span>"
 
   var i = 0;
-  while(i<=messageArray[0].length){
-  	messageArray[0].substring(0,i)
+  while(i<=messageArray5[0].length){
+  	messageArray5[0].substring(0,i)
   	i++
   }
     setTimeout(typewriter5, speed)
@@ -68,13 +73,11 @@ window.addEventListener("load", typewriter5);
 
 
 function typewriter6(){
-  document.querySelector("#message6").innerHTML = messageArray[0].substring(0,textPosition)
+  document.querySelector("#message6").innerHTML = messageArray6[0].substring(0,textPosition)
   + "<span>\u25ae</span>";
 
-  	messageArray.map( ()=>{
-  		messageArray[0].substring(0,textPosition)
-  	})
   
+  if(textPosition++ != messageArray6[0].length)
     setTimeout(typewriter6, speed)
 
 }
